e256fd517b80ed8eb23109c70870cc20be66ffdf
Jacob Stroop <israel941@gmail.com>
2016-02-07 20:14:09 -0500

    Adds documentation

--- Assignment8b_Stroop/Assignment8b_Stroop/Assignment8b_Stroop.cs
+++ Assignment8b_Stroop/Assignment8b_Stroop/Assignment8b_Stroop.cs
@@ -8,20 +8,29 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+/*
+Jacob Stroop
+CIS353 - Assignment 8b
+2/7/16
+*/
+
 namespace Assignment8b_Stroop
 {
     public partial class Assignment8 : Form
     {
+        // constructor calls method to initialize form
         public Assignment8()
         {
             InitializeComponent();
         }
 
+        // Button click method displays message box
         private void btnClickMe_Click(object sender, EventArgs e)
         {
             MessageBox.Show("The Button Has Been Clicked", "Click Me");
         }
 
+        // button click method closes application
         private void btnExit_Click(object sender, EventArgs e)
         {
             Dispose();
